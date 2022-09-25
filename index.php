27da2a9d88e881500cafb77273733bb7c25e210d
niko1511 <niko1511@gmail.com>
2022-09-25 15:01:00 +0200

    tercer commit

--- index.php
+++ index.php
@@ -4,9 +4,19 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Pruebas</title>
+    <title>Home</title>
 </head>
 <body>
-    
+    <h1>Ejemplo Conexión Base de Datos</h1>
+    <form action="connection.php" method="POST">           
+               <input type ="submit" value= "Conectar">
+           </form> <br>
+   <form action="close.php" method="POST">
+   <input type ="submit" value= "Desconectar">
+           </form> <br>
+   <form action="consulta.php" method="POST">
+   <input type ="submit" value= "Consultar">
+           </form> <br>
+ 
 </body>
 </html>
